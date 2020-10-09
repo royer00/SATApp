cc61f77019bf7535622a2159d0c2bd0b4289b148
royer00 <royerkevin@outlook.com>
2020-10-08 23:41:10 -0500

    Add project files.

---
+++ SATApp.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SATApp.UI.MVC.MvcApplication" Language="C#" %>
---
+++ SATApp.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SATApp.UI.MVC.Startup))]
+namespace SATApp.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
