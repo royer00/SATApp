c9f59c0de76727121d8f680acd3dd838517a0078
royer00 <royerkevin@outlook.com>
2020-10-09 00:49:35 -0500

    converted template, added contact form

--- SATApp.UI.MVC/Controllers/HomeController.cs
+++ SATApp.UI.MVC/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using System.Web.Mvc;
-
+﻿using System;
+using System.Net.Mail;
+using System.Web.Mvc;
+using SATApp.UI.MVC.Models;
 namespace SATApp.UI.MVC.Controllers
 {
+
     public class HomeController : Controller
     {
         [HttpGet]
@@ -10,21 +13,54 @@ namespace SATApp.UI.MVC.Controllers
             return View();
         }
 
-        [HttpGet]
-        [Authorize]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
 
-            return View();
-        }
 
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+
 
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+            string message = $"{cvm.Name} has contacted you from {cvm.Email} with a subject of {cvm.Subject}.<br/>" +
+               $"The message is as follows:<br/> {cvm.Message}";
+
+            MailMessage mail = new MailMessage("admin@kevinroyer.net", "royerkevin@outlook.com", $"{System.DateTime.Now.Date}-{ cvm.Subject}", message);
+            mail.Priority = MailPriority.High;
+            mail.IsBodyHtml = true;
+            mail.ReplyToList.Add(cvm.Email);
+            SmtpClient client = new SmtpClient("mail.kevinroyer.net");
+            client.Credentials = new System.Net.NetworkCredential("admin@kevinroyer.net", "P@ssw0rd");
+            client.Port = 8889;
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = "Sorry, there was an error.";
+
+                if (User.IsInRole("Admin"))
+                {
+                    ViewBag.Error = $"Error: {e.StackTrace}";
+                }
+
+                return View(cvm);
+            }
+            return View("EmailConfirmation", cvm);
+
+
+        }
+
     }
 }
+
---
+++ SATApp.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SATApp.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Subject is required")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+
+
+
+    }
+}
